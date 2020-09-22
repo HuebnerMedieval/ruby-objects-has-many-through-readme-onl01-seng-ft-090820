29b343bc732857b946aaf5338a5f1f036cdbbda9
Eli Huebner <elimhuebner@gmail.com>
2020-09-22 19:14:18 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,15 @@
 class Waiter
-
+  attr_accessor :name, :yrs_experience
+  
+  @@all = []
+  
+  def initialze(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
 end
